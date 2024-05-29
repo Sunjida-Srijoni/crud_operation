8ef5e80ae52154cc128e4d77cf036159fe09c5c4
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-05-29 11:53:37 +0600

    index page has been created with a table

--- index.php
+++ index.php
@@ -3,9 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+   
     <title>Home PAge</title>
 </head>
 <body>
-    <h1>Hello from home</h1>
+    <h1 id="main_title">CRUD APPLICATION IN PHP</h1>
+    <div class="container">
+        <h2>ALL STUDENTS</h2>
+    <table class="table table-hover table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+
+            </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>3</td>
+            <td>Anirban</td>
+            <td>Bhowmick</td>
+            <td>23</td>
+
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Rohit</td>
+            <td>Singh</td>
+            <td>26</td>
+
+        </tr>
+    </tbody>
+    </table>
+
+    </div>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
